50cc9aeb323cdfb74c894035a6cae8924bd79636
krad <iam@krad.io>
2018-01-29 23:02:04 -0600

    Initial checkin.  Very very basic HLS VOD playlist parsing

---
+++ test/playlist-parsing-test.js
@@ -0,0 +1,36 @@
+var test  = require('tape')
+var fs    = require('fs')
+var parse = require('../')
+
+test('test that we can test', t=> {
+  t.plan(1)
+  t.ok(1)
+})
+
+test('that we can parse a m3u8 playlist', t=> {
+  t.plan(16)
+  var mock = fs.readFileSync('./test/vod.m3u8').toString()
+  var parsed = parse(mock)
+  t.ok(parsed, 'parsed mock')
+
+  t.ok(parsed.hasOwnProperty('info'), 'has playlist info')
+  t.equals(parsed['info']['targetDuration'], 6, 'target duration correct')
+  t.equals(parsed['info']['version'], 7, 'hls version correct')
+  t.equals(parsed['info']['mediaSequenceNumber'], 1, 'media seq num correct')
+  t.equals(parsed['info']['type'], "VOD", 'playlist type correct')
+
+  t.ok(parsed.hasOwnProperty('segments'), 'has segments')
+  t.ok(Array.isArray(parsed.segments), 'segments is array')
+  t.equals(parsed.segments.length, 6, 'segment count is correct')
+
+  t.equals(true, parsed.segments[0].isIndex, 'first segment is index')
+  t.equals(false, parsed.segments[1].isIndex, 'second segment is NOT index')
+
+  t.equal(6.039592488, parsed.segments[1].duration, 'segment duration correct')
+  t.equal(5.005198766, parsed.segments[2].duration, 'segment duration correct')
+  t.equal(5.005186063, parsed.segments[3].duration, 'segment duration correct')
+  t.equal(5.005201669, parsed.segments[4].duration, 'segment duration correct')
+  t.equal(5.00518475,  parsed.segments[5].duration, 'segment duration correct')
+
+  console.log(parsed)
+})
